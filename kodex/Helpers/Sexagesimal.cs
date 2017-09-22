c75f741979eeee2169397c7bc9ed2f586f1e301d
Brian Koser <brianmkoser@gmail.com>
2017-09-22 13:45:30 -0500

    Implement Sexagesimal to Integer

--- kodex/Helpers/Sexagesimal.cs
+++ kodex/Helpers/Sexagesimal.cs
@@ -34,41 +34,33 @@ namespace kodex.Helpers
 
         public int SexagesimalToInteger(string sexagesimal)
         {
-            int c;
-            int i;
-            int j = sexagesimal.Length;
-            int m = 1;
-            int n = 0;
+            int current;
+            int sign = 1;
+            int result = 0;
 
             if (sexagesimal.Substring(0, 1) == "-")
             {
-                m = -1;
-                j -= 1;
-                sexagesimal = sexagesimal.Substring(1, j);
+                sign = -1;
+                sexagesimal = sexagesimal.Substring(1);
             }
 
-            //        for ($i = 0; $i <$j; $i += 1) { // iterate from first to last char of sexagesimal
-            //$c = ord(sexagesimal[$i]); //  put current ASCII of char into $c
-            //            if ($c >= 48 && $c <= 57) { $c =$c - 48; }
-            //else if ($c >= 65 && $c <= 72) { $c -= 55; }
-            //else if ($c === 73 || $c === 108) { $c = 1; } // typo cap I lower l to 1
-            //else if ($c >= 74 && $c <= 78) { $c -= 56; }
-            //else if ($c === 79) { $c = 0; } // error correct typo capital O to 0
-            //else if ($c >= 80 && $c <= 90) { $c -= 57; }
-            //else if ($c === 95 || $c === 45) { $c = 34; } // _ and dash - to _
-            //else if ($c >= 97 && $c <= 107) { $c -= 62; }
-            //else if ($c >= 109 && $c <= 122) { $c -= 63; }
-            //else { break; } // treat all other noise as end of number
-            //            if (!js() && function_exists('bcadd'))
-            //            {
-            //  $n = bcadd(bcmul(60, $n), $c);
-            //            }
-            //            else
-            //            {
-            //  $n = 60 *$n + $c;
-            //            }
-            //        }
-            return n * m;
+            for (int i = 0; i < sexagesimal.Length; i += 1) // iterate from first to last char of sexagesimal
+            {
+                current = (int)sexagesimal[i]; //  put current ASCII of char into current
+                if (current >= 48 && current <= 57) { current = current - 48; }
+                else if (current >= 65 && current <= 72) { current -= 55; }
+                else if (current == 73 || current == 108) { current = 1; } // error correct typo cap I, lower l to 1
+                else if (current >= 74 && current <= 78) { current -= 56; }
+                else if (current == 79) { current = 0; } // error correct typo capital O to 0
+                else if (current >= 80 && current <= 90) { current -= 57; }
+                else if (current == 95 || current == 45) { current = 34; } // error correct _ and - to _
+                else if (current >= 97 && current <= 107) { current -= 62; }
+                else if (current >= 109 && current <= 122) { current -= 63; }
+                else { break; } // treat all other noise as end of number
+
+                result = 60 * result + current;
+            }
+            return result * sign;
         }
     }
 }
